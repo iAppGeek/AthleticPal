109e1b273b7e8a98cef126aea8ba0b67bc6e5b54
Anthony <anthony.ladas@gmail.com>
2019-01-11 23:46:11 +0000

    add basic app and deployed site

---
+++ docs/precache-manifest.ecfa86749fc371c79e386c99f7c21c17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "51394513c3b78e6983bd",
+    "url": "/static/js/main.51394513.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "51394513c3b78e6983bd",
+    "url": "/static/css/main.f9482c2d.chunk.css"
+  },
+  {
+    "revision": "80ac37e8e40baceccd8a6eb5b93fc2f9",
+    "url": "/index.html"
+  }
+];
