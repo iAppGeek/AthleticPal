28e23e5268e7e8163402e7aeda793eb73b788522
Anthony <anthony.ladas@gmail.com>
2019-01-12 01:06:04 +0000

    add some dummy bootstrap things

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PageHeader from './components/PageHeader';
+import PageFooter from './components/PageFooter';
+import {Button, Jumbotron} from "react-bootstrap";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <PageHeader />
+
+          <Jumbotron>
+              <h1>Hello, world!</h1>
+              <p>
+                  This is a simple hero unit, a simple jumbotron-style component for calling
+                  extra attention to featured content or information.
+              </p>
+              <p>
+                  <Button bsStyle="primary">Learn more</Button>
+              </p>
+          </Jumbotron>
+
+        <PageFooter />
       </div>
     );
   }
---
+++ src/components/PageFooter.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const PageFooter = (props: any) => {
+    return (
+        <p>Footer Text</p>
+    )
+}
+
+export default PageFooter;
